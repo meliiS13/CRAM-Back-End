8dacb7799ae40cec982313c515a6873d443b944b
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-05-27 12:25:54 -0300

    t

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LugaresController } from './lugares/lugares.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, LugaresController],
   providers: [AppService],
 })
 export class AppModule {}
