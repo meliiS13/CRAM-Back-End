daa64a4a0f8805f6ef40fd7e7d1336d5b598704c
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-06-02 09:04:17 -0300

    db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LugaresController } from './lugares/lugares.controller';
 import { UsuarioController } from './usuario/usuario.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController, LugaresController, UsuarioController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlserver',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
