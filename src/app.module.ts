a75156fc450ec0f3e5d089f7ba8716fc249b29fc
sofiruba <102734675+sofiruba@users.noreply.github.com>
2022-08-10 11:53:05 -0300

    config y enviroment para la db

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,26 +9,36 @@ import { Lugar } from './lugares/lugares.entity';
 import { Review } from './reviews/Reviews.entity';
 import { ReviewModule } from './reviews/Reviews.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 
 @Module({
   providers: [AppService],
   controllers: [AppController],
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
     LugarModule,
     UsuariosModule,
     ReviewModule,
     AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'mssql',
-      host: 'localhost',
-      port: 1434,
-      username: 'alumno',
-      password: 'alumno', 
-      database: 'CRAM',
-      entities: [Usuario, Lugar, Review],
-      synchronize: false,
-    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mssql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DN_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    })
   ],
 
 })
